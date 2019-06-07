f63e622226ba6a67eecab8a803c092dd9fc193e6
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2019-06-07 11:24:03 +0530

    Fixed all the space/tab indentation errors

--- deep_q_network_real_test.py
+++ deep_q_network_real_test.py
@@ -13,6 +13,7 @@ import sys
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+
 # import collect_code.pycontrol as ur
 
 ###################################################################################
@@ -62,35 +63,40 @@ READ_NETWORK_DIR = None
 ACTION_NORM = None
 TEST_RESULT_PATH = None
 
-
 # used in pre-process the picture
 RESIZE_WIDTH = 128
 RESIZE_HEIGHT = 128
 
 # parameters used in testing
-ACTIONS = 5 # number of valid actions
+ACTIONS = 5  # number of valid actions
 PAST_FRAME = 3
+
 
 ###################################################################################
 # Functions
 ###################################################################################
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.01)
+    initial = tf.truncated_normal(shape, stddev=0.01)
     return tf.Variable(initial)
+
 
 def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding="SAME")
+
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-def space_tiling(x): # expand from [None, 64] to [None, 4, 4, 64]
+
+def space_tiling(x):  # expand from [None, 64] to [None, 4, 4, 64]
     x = tf.expand_dims(tf.expand_dims(x, 1), 1)
     return tf.tile(x, [1, 4, 4, 1])
+
 
 '''
 createNetwork - set the structure of CNN
@@ -130,40 +136,40 @@ training = tf.placeholder_with_default(False, name='training', shape=())
 h_conv1 = conv2d(s, W_conv1, 4) + b_conv1
 h_bn1 = tf.layers.batch_normalization(h_conv1, axis=-1, training=training, momentum=0.9)
 h_relu1 = tf.nn.relu(h_bn1)
-h_pool1 = max_pool_2x2(h_relu1) # [None, 16, 16, 32]
+h_pool1 = max_pool_2x2(h_relu1)  # [None, 16, 16, 32]
 
 h_conv2 = conv2d(h_pool1, W_conv2, 2) + b_conv2
 h_bn2 = tf.layers.batch_normalization(h_conv2, axis=-1, training=training, momentum=0.9)
 h_relu2 = tf.nn.relu(h_bn2)
-h_pool2 = max_pool_2x2(h_relu2) # [None, 4, 4, 64]
+h_pool2 = max_pool_2x2(h_relu2)  # [None, 4, 4, 64]
 
 h_fc_info = tf.matmul(past_info, W_fc_info) + b_fc_info
 h_bn_info = tf.layers.batch_normalization(h_fc_info, axis=-1, training=training, momentum=0.9)
-h_relu_info = tf.nn.relu(h_bn_info) # [None, 64]
+h_relu_info = tf.nn.relu(h_bn_info)  # [None, 64]
 
-info_add = space_tiling(h_relu_info) # [None, 4, 4, 64]
-layer3_input = tf.concat([h_pool2, info_add], 3) # [None, 4, 4, 128]
+info_add = space_tiling(h_relu_info)  # [None, 4, 4, 64]
+layer3_input = tf.concat([h_pool2, info_add], 3)  # [None, 4, 4, 128]
 h_conv3 = conv2d(layer3_input, W_conv3, 1) + b_conv3
 h_bn3 = tf.layers.batch_normalization(h_conv3, axis=-1, training=training, momentum=0.9)
-h_relu3 = tf.nn.relu(h_bn3) # [None, 4, 4, 64]
+h_relu3 = tf.nn.relu(h_bn3)  # [None, 4, 4, 64]
 # h_pool3 = max_pool_2x2(h_relu3) # [None, 2, 2, 64]
 
 h_conv4 = conv2d(h_relu3, W_conv4, 1) + b_conv4
 h_bn4 = tf.layers.batch_normalization(h_conv4, axis=-1, training=training, momentum=0.9)
-h_relu4 = tf.nn.relu(h_bn4) # [None, 4, 4, 64]
-h_pool4 = max_pool_2x2(h_relu4) # [None, 2, 2, 64]
+h_relu4 = tf.nn.relu(h_bn4)  # [None, 4, 4, 64]
+h_pool4 = max_pool_2x2(h_relu4)  # [None, 2, 2, 64]
 
-h_pool4_flat = tf.reshape(h_pool4, [-1, 256]) # [None, 256]
+h_pool4_flat = tf.reshape(h_pool4, [-1, 256])  # [None, 256]
 
 h_fc1 = tf.matmul(h_pool4_flat, W_fc1) + b_fc1
 h_bn_fc1 = tf.layers.batch_normalization(h_fc1, axis=-1, training=training, momentum=0.9)
-h_relu_fc1 = tf.nn.relu(h_bn_fc1) # [None, 256]
-    
+h_relu_fc1 = tf.nn.relu(h_bn_fc1)  # [None, 256]
+
 h_fc2 = tf.matmul(h_relu_fc1, W_fc2) + b_fc2
 h_bn_fc2 = tf.layers.batch_normalization(h_fc2, axis=-1, training=training, momentum=0.9)
-h_relu_fc2 = tf.nn.relu(h_bn_fc2) # [None, 256]
+h_relu_fc2 = tf.nn.relu(h_bn_fc2)  # [None, 256]
 # readout layer
-readout = tf.matmul(h_relu_fc2, W_fc3) + b_fc3 # [None, 5]
+readout = tf.matmul(h_relu_fc2, W_fc3) + b_fc3  # [None, 5]
 
 '''
 Neural Network Definitions --- not necessary in test
@@ -194,6 +200,8 @@ testNetwork - test the training performance, calculate the success rate
 Input: s, action,readout
 Return: success rate
 '''
+
+
 def testNetwork():
     # init the real test environment
     test_env = env.FocusEnv([TEST_RESULT_PATH, None, FLAGS.MAX_STEPS, FLAGS.MIN_ANGLE, FLAGS.MAX_ANGLE])
@@ -212,91 +220,97 @@ def testNetwork():
         if FLAGS.VERSION:
             checkpoint = tf.train.get_checkpoint_state(READ_NETWORK_DIR)
             if checkpoint and checkpoint.model_checkpoint_path:
-                    saver.restore(sess, checkpoint.model_checkpoint_path)
-                    print("Successfully loaded:", checkpoint.model_checkpoint_path)
+                saver.restore(sess, checkpoint.model_checkpoint_path)
+                print("Successfully loaded:", checkpoint.model_checkpoint_path)
             else:
-                    print("Could not find old network weights")
-    	
-	success_cnt = 0.0
-	total_steps = 0.0
+                print("Could not find old network weights")
+
+        success_cnt = 0.0
+        total_steps = 0.0
         # start test
-	for test in range(FLAGS.TEST_ROUND):
-	    init_angle, init_img_path = test_env.reset()
-	                
-	    # generate the first state, a_past is 0
-	    img_t = cv2.imread(init_img_path)
-	    img_t = cv2.cvtColor(cv2.resize(img_t, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY)
-	    s_t = np.stack((img_t, img_t, img_t) , axis=2)
+        for test in range(FLAGS.TEST_ROUND):
+            init_angle, init_img_path = test_env.reset()
+
+            # generate the first state, a_past is 0
+            img_t = cv2.imread(init_img_path)
+            img_t = cv2.cvtColor(cv2.resize(img_t, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY)
+            s_t = np.stack((img_t, img_t, img_t), axis=2)
             action_t = np.stack((0.0, 0.0, 0.0), axis=0)
-	    past_info_t = action_t
-	    step = 1
-	    # start 1 episode
-	    while True:
-	        # run the network forwardly
-	        readout_t = readout.eval(feed_dict={
-			s : [s_t], 
-			past_info : [past_info_t],
-			training : False})[0]
-        	print(past_info_t)
-		print(readout_t)
-		# determine the next action
-		action_index = np.argmax(readout_t)
-		a_input = test_env.actions[action_index]
-		# run the selected action and observe next state and reward
-		angle_new, img_path_t1, terminal, success = test_env.test_step(a_input)
+            past_info_t = action_t
+            step = 1
+            # start 1 episode
+            while True:
+                # run the network forwardly
+                readout_t = readout.eval(feed_dict={
+                    s: [s_t],
+                    past_info: [past_info_t],
+                    training: False})[0]
+                print(past_info_t)
+                print(readout_t)
+                # determine the next action
+                action_index = np.argmax(readout_t)
+                a_input = test_env.actions[action_index]
+                # run the selected action and observe next state and reward
+                angle_new, img_path_t1, terminal, success = test_env.test_step(a_input)
 
-		if terminal:
-			# calculate
-		        success_cnt += int(success) # only represents the rate of active terminate
-		        total_steps += step
-			# get the final focus
-			focus_end = TENG(img_path_t1)
-                        print("test ", test, "ends at ", focus_end)
-		        break
-		            
-		img_t1 = cv2.imread(img_path_t1)
-		img_t1 = cv2.cvtColor(cv2.resize(img_t1, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY)
-		img_t1 = np.reshape(img_t1, (RESIZE_WIDTH, RESIZE_HEIGHT, 1)) # reshape, ready for insert
-		action_new = np.reshape(a_input/ACTION_NORM, (1,))
-		s_t1 = np.append(img_t1, s_t[:, :, :PAST_FRAME-1], axis=2)
-		action_t1 = np.append(action_new, action_t[:PAST_FRAME-1], axis=0)
-	        past_info_t1 = action_t1
-		# print test info
-		print("TEST EPISODE", test, "/ TIMESTEP", step, \
-			"/ CURRENT ANGLE", test_env.cur_state, "/ ACTION", a_input)
+                if terminal:
+                    # calculate
+                    success_cnt += int(success)  # only represents the rate of active terminate
+                    total_steps += step
+                    # get the final focus
+                    focus_end = TENG(img_path_t1)
+                    print("test ", test, "ends at ", focus_end)
+                    break
 
-		# update
-		s_t = s_t1
-		action_t = action_t1
-		past_info_t = action_t
-		step += 1
+                img_t1 = cv2.imread(img_path_t1)
+                img_t1 = cv2.cvtColor(cv2.resize(img_t1, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY)
+                img_t1 = np.reshape(img_t1, (RESIZE_WIDTH, RESIZE_HEIGHT, 1))  # reshape, ready for insert
+                action_new = np.reshape(a_input / ACTION_NORM, (1,))
+                s_t1 = np.append(img_t1, s_t[:, :, :PAST_FRAME - 1], axis=2)
+                action_t1 = np.append(action_new, action_t[:PAST_FRAME - 1], axis=0)
+                past_info_t1 = action_t1
+                # print test info
+                print("TEST EPISODE", test, "/ TIMESTEP", step, \
+                      "/ CURRENT ANGLE", test_env.cur_state, "/ ACTION", a_input)
 
-        sess.close() # end session
+                # update
+                s_t = s_t1
+                action_t = action_t1
+                past_info_t = action_t
+                step += 1
+
+        sess.close()  # end session
 
     # calculate final return results
     success_rate = success_cnt / FLAGS.TEST_ROUND
     step_cost = total_steps / FLAGS.TEST_ROUND
     print("success_rate:", success_rate, "step per episode:", step_cost)
     # record and focus
-    record_end_focus(success, step) 
+    record_end_focus(success, step)
     return
+
 
 '''
 Tenengrad
 '''
+
+
 def TENG(img_path):
-    img = cv2.imread(img_path) # read pic
-    img = cv2.cvtColor(cv2.resize(img, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY) # resize
+    img = cv2.imread(img_path)  # read pic
+    img = cv2.cvtColor(cv2.resize(img, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY)  # resize
     guassianX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
     guassianY = cv2.Sobel(img, cv2.CV_64F, 1, 0)
     return np.mean(guassianX * guassianX + guassianY * guassianY)
 
+
 '''
 record_end_focus
 '''
+
+
 def record_end_focus(success_rate, step_cost):
     # write success rate and average steps to txt file
-    txtFile = os.path.join(TEST_RESULT_PATH, 'result.txt') 
+    txtFile = os.path.join(TEST_RESULT_PATH, 'result.txt')
     with open(txtFile, 'w') as f:
         Data = "success rate:" + str(success_rate) + " step per episode:" + str(step_cost)
         f.write(Data)
@@ -311,64 +325,70 @@ def record_end_focus(success_rate, step_cost):
     for root, dirs, files in os.walk(TEST_RESULT_PATH):
         for dir in dirs:
             epiDirs.append(dir)
-    epiDirs.sort(key=lambda obj:int(obj)) # only process dirs, sort episode dirs
+    epiDirs.sort(key=lambda obj: int(obj))  # only process dirs, sort episode dirs
 
     # walk through the folder
     for p in range(len(epiDirs)):
-        imageList = [] # clear list
+        imageList = []  # clear list
         # get into one episode directory
-	for root, dirs, files in os.walk(os.path.join(TEST_RESULT_PATH, epiDirs[p])):
-        	for file in files:
-            		if os.path.splitext(file)[1] == '.jpg':
-                		imageList.append(file)
-	# print(imageList)
-	# sort
-	imageList.sort(key=lambda obj:int(obj.split('_')[0])) # sort image list
-	fList = [] # clear the list
+        for root, dirs, files in os.walk(os.path.join(TEST_RESULT_PATH, epiDirs[p])):
+            for file in files:
+                if os.path.splitext(file)[1] == '.jpg':
+                    imageList.append(file)
+        # print(imageList)
+        # sort
+        imageList.sort(key=lambda obj: int(obj.split('_')[0]))  # sort image list
+        fList = []  # clear the list
 
-	# walk through the images	
-	for i in range(len(imageList)):
-		img_path = TEST_RESULT_PATH + '/' + epiDirs[p] + '/' + imageList[i]
-		print("processing %s" %img_path)
-		focus = TENG(img_path)
-		fList.append(focus)
-	# plot focus changing in one episode
-	plot_focus_in_one_episode(os.path.join(TEST_RESULT_PATH, epiDirs[p]), p, fList)
-	endfList.append(fList[-1]) # add the final focus to endfList
-	stepList.append(len(imageList))
+        # walk through the images
+        for i in range(len(imageList)):
+            img_path = TEST_RESULT_PATH + '/' + epiDirs[p] + '/' + imageList[i]
+            print("processing %s" % img_path)
+            focus = TENG(img_path)
+            fList.append(focus)
+        # plot focus changing in one episode
+        plot_focus_in_one_episode(os.path.join(TEST_RESULT_PATH, epiDirs[p]), p, fList)
+        endfList.append(fList[-1])  # add the final focus to endfList
+        stepList.append(len(imageList))
     plot_histogram(endfList, stepList)
     return
+
 
 '''
 plot focus in one episode
 '''
+
+
 def plot_focus_in_one_episode(epipath, p, fList):
     plt.figure()
     plt.plot(fList, 'bx-')
     plt.xlabel("ops")
     plt.ylabel("Focus Measure")
     plt.title("Focus Changing in episode {}".format(p))
-    plt.savefig(epipath + "/f_change", dpi=600)	
+    plt.savefig(epipath + "/f_change", dpi=600)
     # plt.show() 
+
 
 '''
 plot histogram of end focus measure and steps
 '''
+
+
 def plot_histogram(endfList, stepList):
     # plot focus histogram
     plt.figure()
-    f = [94647,85677,93443,100003,99992,88889,99902,110029,89898,92201]
+    f = [94647, 85677, 93443, 100003, 99992, 88889, 99902, 110029, 89898, 92201]
     X = [15000, 30000, 45000, 60000, 75000, 90000, 105000, 120000]
-    X1 = [X[i]+6300 for i in range(len(X))]
+    X1 = [X[i] + 6300 for i in range(len(X))]
     Y1 = [0 for i in range(len(X))]
     Y2 = [0 for i in range(len(X))]
     for i in range(len(endfList)):
-        Y1[int(endfList[i]/15000)] += 1
-        Y2[int(f[i]/15000)] += 1
+        Y1[int(endfList[i] / 15000)] += 1
+        Y2[int(f[i] / 15000)] += 1
     print(endfList, f)
     print(Y1, Y2)
-    plt.bar(X,Y1,width = 6000,facecolor = 'lightskyblue', label='virtually-trained model') 
-    plt.bar(X1,Y2,width = 6000,facecolor = 'yellowgreen', label='real-trained model')
+    plt.bar(X, Y1, width=6000, facecolor='lightskyblue', label='virtually-trained model')
+    plt.bar(X1, Y2, width=6000, facecolor='yellowgreen', label='real-trained model')
     # plt.axis([0, 130000, 0, 6])
     plt.xlabel("Focus Positions")
     plt.ylabel("Number of Cases")
@@ -400,15 +420,18 @@ def plot_histogram(endfList, stepList):
     plt.show()
     '''
 
+
 '''
 main
 '''
+
+
 def main(_):
     global TRAIN_DIR, READ_NETWORK_DIR, ACTION_NORM, env, TEST_RESULT_PATH
     # import env
     env = __import__(FLAGS.ENV_PATH)
     # normalize the action
-    ACTION_NORM = 0.3*env.TIMES
+    ACTION_NORM = 0.3 * env.TIMES
 
     # specify the important directories
     TRAIN_DIR = PATH + "/training/" + FLAGS.VERSION
@@ -422,11 +445,11 @@ def main(_):
 
     # set GPU
     os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.GPU_LIST
-    
+
     # start real test!
     # testNetwork()
     record_end_focus(0.7, 9.8)
-    
 
-if __name__ == '__main__':   
-	tf.app.run()
+
+if __name__ == '__main__':
+    tf.app.run()
--- realenv_train.py
+++ realenv_train.py
@@ -3,7 +3,7 @@
 # Include the state transfer. All the image processing are not here.
 # Modified by xfyu on May 19, 2018.
 ##################################################################
-#!/usr/bin/env python
+# !/usr/bin/env python
 import os
 import random
 import time
@@ -11,6 +11,7 @@ import cv2
 import numpy
 import sys
 import collect_code.pycontrol as ur
+
 #################################################################
 # Important global parameters
 #################################################################
@@ -31,39 +32,40 @@ SUCCESS_THRES = 0.9
 # VERY IMPORTANT!!!
 TIMES = 9
 # actions
-COARSE_POS = 0.3*TIMES
+COARSE_POS = 0.3 * TIMES
 FINE_POS = 0.3
 TERMINAL = 0
 FINE_NEG = -0.3
-COARSE_NEG = -0.3*TIMES
+COARSE_NEG = -0.3 * TIMES
 # pic resize
 RESIZE_WIDTH = 128
 RESIZE_HEIGHT = 128
 
+
 # only for train!
-class FocusEnvTrain(): # one class for one folder
+class FocusEnvTrain():  # one class for one folder
     def __init__(self, info, pathinfo):  # info:[MAX_STEPS, MIN_ANGLE, MAX_ANGLE]
-                                         # pathinfo:[MAIN_DIR, REFERENCE_DIR]
+        # pathinfo:[MAIN_DIR, REFERENCE_DIR]
         # COARSE NEG, FINE NEG, TERMINAL, FINE POS, COARSE POS
         self.actions = [COARSE_NEG, FINE_NEG, TERMINAL, FINE_POS, COARSE_POS]
         self.max_steps, self.min_angle, self.max_angle = info[0], info[1], info[2]
         self.main_dir, self.reference_path = pathinfo[0], pathinfo[1]
 
         self.cur_state = 0.0  # initial with 0.0 
-        self.get_max_focus() # set focus points
+        self.get_max_focus()  # set focus points
 
-        ur.system_init() # hardware init for once
+        ur.system_init()  # hardware init for once
 
     def __del__(self):
-        ur.system_close() # hardware close for once
+        ur.system_close()  # hardware close for once
 
-    def reset(self, other_env_state=-1.0): # -1.0 an impossible value
+    def reset(self, other_env_state=-1.0):  # -1.0 an impossible value
         # record the final state of last episode or last state of the other env
-        if other_env_state == -1.0: # reload last state from the same env
+        if other_env_state == -1.0:  # reload last state from the same env
             last_state = self.cur_state
-            last_state = round(last_state, 2) # just in case
+            last_state = round(last_state, 2)  # just in case
         else:
-            last_state = round(other_env_state, 2) # reload last state from the other env
+            last_state = round(other_env_state, 2)  # reload last state from the other env
         # randomly decide the new initial state, the angle here is not accurate, just for random actions
         state = random.random() * (self.max_angle - self.min_angle)
         self.cur_state = self.min_angle + state
@@ -79,7 +81,6 @@ class FocusEnvTrain(): # one class for one folder
         ur.camera_take_pic(pic_name)
 
         return self.cur_state, init_path
-    
 
     '''
     step - regulations of transfering between states
@@ -90,106 +91,110 @@ class FocusEnvTrain(): # one class for one folder
             reward - reward of this single operation
             terminal - True or False
     '''
-    def step(self, input_action): # action is the angle to move
+
+    def step(self, input_action):  # action is the angle to move
         self.cur_step = self.cur_step + 1
         # fugure out the place to save pic
         pic_name = os.path.join(self.main_dir, 'pic.jpg')
-        
+
         # update self.cur_state 
-    	self.cur_state = self.cur_state + input_action
+        self.cur_state = self.cur_state + input_action
         self.cur_state = round(self.cur_state, 2)
 
         # real movement
         # move to the next state and take a pic
-        if abs(input_action) > 1: # COARSE
+        if abs(input_action) > 1:  # COARSE
             ur.change_focus_mode(ur.COARSE)
-        elif abs(input_action) > 0.0: # fine
+        elif abs(input_action) > 0.0:  # fine
             ur.change_focus_mode(ur.FINE)
-        if input_action > 0: # UP
+        if input_action > 0:  # UP
             ur.send_movej_screw(ur.UP)
-        elif input_action < 0: # DOWN
+        elif input_action < 0:  # DOWN
             ur.send_movej_screw(ur.DOWN)
         ur.camera_take_pic(pic_name)
         next_image_path = pic_name
-	# print("step take pic complete!")        
+        # print("step take pic complete!")
 
-	# calculate focus
+        # calculate focus
         focus = TENG(pic_name)
 
         # special termination
         if self.cur_state > self.max_angle or self.cur_state < self.min_angle:
-                return self.cur_state, next_image_path, FAILURE_REWARD + self.get_reward(focus), True
+            return self.cur_state, next_image_path, FAILURE_REWARD + self.get_reward(focus), True
 
         # choose to terminate
         if input_action == TERMINAL:
-                if focus > self.success_focus:
-            		return self.cur_state, next_image_path, SUCCESS_REWARD, True
-            	return self.cur_state, next_image_path, FAILURE_REWARD + self.get_reward(focus), True
+            if focus > self.success_focus:
+                return self.cur_state, next_image_path, SUCCESS_REWARD, True
+            return self.cur_state, next_image_path, FAILURE_REWARD + self.get_reward(focus), True
 
-    	# special case - failure
-    	if self.cur_step >= self.max_steps:
-            	return self.cur_state, next_image_path, FAILURE_REWARD + self.get_reward(focus), True
+        # special case - failure
+        if self.cur_step >= self.max_steps:
+            return self.cur_state, next_image_path, FAILURE_REWARD + self.get_reward(focus), True
 
         # normal case
         return self.cur_state, next_image_path, self.get_reward(focus), False
 
-	
     '''
     get_max_focus - get the max focus for reference
     '''
+
     def get_max_focus(self):
         cur_angle = self.min_angle
         max_focus = 0.0
-    	# walk through every images in the directory
+        # walk through every images in the directory
         while cur_angle <= self.max_angle:
-            pic_name = os.path.join(self.reference_path, str(cur_angle)+'.jpg')
-            print("calculating %s" %pic_name)
+            pic_name = os.path.join(self.reference_path, str(cur_angle) + '.jpg')
+            print("calculating %s" % pic_name)
             # read and calculate focus
             cur_focus = TENG(pic_name)
             if cur_focus > max_focus:
-                    max_focus = cur_focus
-            cur_angle += 0.3 # move to the next picture
+                max_focus = cur_focus
+            cur_angle += 0.3  # move to the next picture
         print("max focus is", max_focus)
         # self.change_point_focus = max_focus * CHANGE_POINT_THRES
         self.success_focus = max_focus * SUCCESS_THRES
         self.max_focus = max_focus
         return max_focus
 
-
     '''
     get_reward - reward determination
     generate reward from the current taking pictures, should be less than 0
     '''
-    def get_reward(self, cur_focus):
-    	reward = (cur_focus - self.success_focus) * FOCUS_REWARD_NORM / self.max_focus
-    	print("max", self.success_focus, "cur", cur_focus, "reward is", reward)
-    	# self.last_focus = cur_focus # update
-    	return reward # return
 
+    def get_reward(self, cur_focus):
+        reward = (cur_focus - self.success_focus) * FOCUS_REWARD_NORM / self.max_focus
+        print("max", self.success_focus, "cur", cur_focus, "reward is", reward)
+        # self.last_focus = cur_focus # update
+        return reward  # return
 
     '''
     move - move the ur3 from start_angle to end_angle
     '''
+
     def move(self, start_angle, end_angle):
-	print("move from", start_angle, "to", end_angle)
-    	if abs(end_angle - start_angle) > 10:
-		delta_angle = (end_angle - start_angle) / TIMES
-                delta_angle = round(delta_angle, 2)
-    		print("COARSE move", delta_angle)
-    		ur.change_focus_mode(ur.COARSE)
-    		ur.move_from_to(delta_angle)
-    	else:
-		delta_angle = round(end_angle - start_angle, 2)
-		print("FINE move", delta_angle)
-		ur.change_focus_mode(ur.FINE)
-		ur.move_from_to(delta_angle)
+        print("move from", start_angle, "to", end_angle)
+        if abs(end_angle - start_angle) > 10:
+            delta_angle = (end_angle - start_angle) / TIMES
+            delta_angle = round(delta_angle, 2)
+            print("COARSE move", delta_angle)
+            ur.change_focus_mode(ur.COARSE)
+            ur.move_from_to(delta_angle)
+        else:
+            delta_angle = round(end_angle - start_angle, 2)
+            print("FINE move", delta_angle)
+            ur.change_focus_mode(ur.FINE)
+            ur.move_from_to(delta_angle)
+
 
 '''
 Tenengrad
 '''
+
+
 def TENG(img_path):
-    img = cv2.imread(img_path) # read pic
-    img = cv2.cvtColor(cv2.resize(img, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY) # resize
+    img = cv2.imread(img_path)  # read pic
+    img = cv2.cvtColor(cv2.resize(img, (RESIZE_WIDTH, RESIZE_HEIGHT)), cv2.COLOR_BGR2GRAY)  # resize
     guassianX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
     guassianY = cv2.Sobel(img, cv2.CV_64F, 1, 0)
     return numpy.mean(guassianX * guassianX + guassianY * guassianY)
